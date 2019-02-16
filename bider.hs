ea0add305409ce902f24a7de5be545e6c6d788bd
iliaspap123 <iliaspap97@gmail.com>
2019-02-16 16:28:43 +0200

    3to

---
+++ bider.hs
@@ -0,0 +1,32 @@
+import Data.List
+
+
+bfs ls = depth1 (initial (length ls)) (initial (length ls)) ls [ls] [(sort ls)]
+
+
+initial n = [[x] | x<-[2..n]]
+
+depth1 (f1:fifo1) fifo2 ls visited1 visited2 | (elem (last (visualize ls f1)) visited2 == True) = (f1,1)
+                        | (elem (last (visualize ls f1)) visited1 == True) = depth2 fifo1 fifo2 ls visited1 visited2
+                        | otherwise = depth2 (fifo1++(next_elem f1 (length ls))) fifo2 ls ((last (visualize ls f1)):visited1) visited2
+
+depth2 fifo1 (f2:fifo2) ls visited1 visited2 | (elem (last (visualize (sort ls) f2)) visited1 == True) = (f2,2)
+                        | (elem (last (visualize ls f2)) visited2 == True) =  depth1 fifo1 fifo2 ls visited1 visited2
+                        | otherwise = depth1 fifo1 (fifo2++(next_elem f2 (length ls))) ls visited1 ((last (visualize ls ([1..(length ls)]))):visited2)
+
+next_elem f n = [f++[x] | x<-[2..n], x /= (last f)]
+
+
+nreve n xs = (reverse (take n xs)) ++ (drop n xs)
+
+visualize xs [] = [xs]
+visualize xs (n:ns) = [xs] ++ (visualize (nreve n xs) ns)
+
+goal a ls= checkList (last (visualize ls a))
+
+
+
+checkList :: (Ord a) => [a] -> Bool
+checkList [] = True
+checkList [x] = True
+checkList (x:y:xs) = x <= y && checkList (y:xs)
--- pan.hs
+++ pan.hs
@@ -1,11 +1,37 @@
+import Data.List
 
+data Tree a = Empty | Node a [Tree a]
+
+
+--depth (Node a xs) ls| goal a ls= a
+--                  | otherwise = False
+
+next_elem f n = [f++[x] | x<-[2..n], x /= (last f)]
+
+
+
+depth (f:fifo) ls visited | goal f ls= f
+                        | (elem (last (visualize ls f)) visited == True) =  depth fifo ls visited
+                        | otherwise = depth (fifo++(next_elem f (length ls))) ls ((last (visualize ls f)):visited)
+
+
+goal a ls= checkList (last (visualize ls a))
+
+
+initial n = [[x] | x<-[2..n]]
+
+bfs ls = depth (initial (length ls)) ls []
+
+checkList :: (Ord a) => [a] -> Bool
+checkList [] = True
+checkList [x] = True
+checkList (x:y:xs) = x <= y && checkList (y:xs)
 
 nreve n xs = (reverse (take n xs)) ++ (drop n xs)
 
 visualize xs [] = [xs]
 visualize xs (n:ns) = [xs] ++ (visualize (nreve n xs) ns)
 
---flop 0 xs r = reverse r
 
 flop n xs r | n == 1 = reverse r
             | ((max_index n xs) /= 1) = flop (n-1) (nreve n (nreve (max_index n xs) xs)) (n:((max_index n xs):r))
