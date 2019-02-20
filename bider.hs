577ca84b2e21d053f0f71c1f57191165e90ce8d2
iliaspap123 <iliaspap97@gmail.com>
2019-02-20 11:37:53 +0200

    bidir

--- bider.hs
+++ bider.hs
@@ -1,17 +1,30 @@
 import Data.List
 import Data.Tuple
 
-bidirectional ls = depth1 (initial (length ls)) (initial (length ls)) ls [(ls,[])] [((sort ls),[])]
+--bidirectional ls = depth1 (initial (length ls)) (initial (length ls)) ls [(ls,[])] [((sort ls),[])]
+
+
+bidirectional ls = d1 (initial (length ls)) (initial (length ls)) 1 ls [(ls,[])] [((sort ls),[])]
+
+
+d1 (f1:fifo1) fifo2 dep ls visited1 visited2 | length f1 > dep = d2 (f1:fifo1) fifo2 dep  ls visited1 []
+                                            | (length f1 == dep) && (elem (last (visualize ls f1)) ( map (\x->fst x) visited2 )== True) = f1++(reverse (find_tuple (last (visualize ls f1)) visited2))
+                                            | otherwise = d1 (fifo1++(reverse (next_elem f1 (length ls)))) fifo2 dep ls (((last (visualize ls f1)),f1):visited1) visited2
+
+d2 fifo1 (f2:fifo2) dep ls visited1 visited2 | length f2 > dep = d1 fifo1 (f2:fifo2) (dep+1)  ls [] visited2
+                                | (length f2 == dep) && (elem (last (visualize (sort ls) f2)) ( map (\x->fst x) visited1 )== True) = f2++(reverse (find_tuple (last (visualize (sort ls) f2)) visited1))
+                                | otherwise = d2 fifo1 (fifo2++(reverse (next_elem f2 (length ls)))) dep ls visited1 (((last (visualize (sort ls) f2)),f2):visited2)
 
 
 initial n = [[x] | x<-[2..n]]
 
 depth1 (f1:fifo1) fifo2 ls visited1 visited2 | (elem (last (visualize ls f1)) ( map (\x->fst x) visited2 )== True) = f1++(reverse (find_tuple (last (visualize ls f1)) visited2))
-                        | (elem (last (visualize ls f1)) ( map (\x->fst x) visited1 ) == True) = depth2 fifo1 fifo2 ls visited1 visited2
+--                          |length f1 == 10 = []
+--                        | (elem (last (visualize ls f1)) ( map (\x->fst x) visited1 ) == True) = depth2 fifo1 fifo2 ls visited1 visited2
                         | otherwise = depth2 (fifo1++(next_elem f1 (length ls))) fifo2 ls (visited1++[((last (visualize ls f1)),f1)]) visited2
 
 depth2 fifo1 (f2:fifo2) ls visited1 visited2 | (elem (last (visualize (sort ls) f2)) ( map (\x->fst x) visited1 ) == True) = ((find_tuple (last (visualize (sort ls) f2)) visited1))++( reverse f2)
-                        | (elem (last (visualize (sort ls) f2)) ( map (\x->fst x) visited2 ) == True) =  depth1 fifo1 fifo2 ls visited1 visited2
+--                        | (elem (last (visualize (sort ls) f2)) ( map (\x->fst x) visited2 ) == True) =  depth1 fifo1 fifo2 ls visited1 visited2
                         | otherwise = depth1 fifo1 (fifo2++(next_elem f2 (length ls))) ls visited1 (visited2++[((last (visualize (sort ls) f2)),f2)])
 
 next_elem f n = [f++[x] | x<-[2..n], x /= (last f)]
