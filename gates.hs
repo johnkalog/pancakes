27f78508d731a343ef522e20d9ee48d6a2d9b554
iliaspap123 <iliaspap97@gmail.com>
2019-02-22 18:09:27 +0200

    gates2

--- gates.hs
+++ gates.hs
@@ -10,6 +10,12 @@ category (l:ls) | (ls == []) = 8
                 | otherwise = 10
 
 
+gates (l:ls)
+            | category (l:ls) == 1 = my_merge ( my_flip (l:ls) ((which_flip (l:ls) (head l) 1) -1))
+            | category (l:ls) == 2 = my_merge ( my_flip (l:ls) ((which_flip (l:ls) (head l) 1) -1))
+            | category (l:ls) == 3 = my_merge ( my_flip (l:ls) (min ((which_exac (l:ls) ((head l)-1) 1) -1) (which_exac (l:ls) ((head l)-1) 1) -1))
+            | otherwise = [[category (l:ls)]]
+
 next_abj x [] = 0
 next_abj x (l:ls) | ((head l) == x) = (length l)
                 | otherwise = next_abj x ls
@@ -19,9 +25,24 @@ is_pos f x (l:ls) | (length l == 1) = is_pos f x ls
                   | ((f l) == x) = True
                   | otherwise = is_pos f x ls
 my_merge [] = []
-my_merge (l1:(l2:l)) | ((last l1)+1 == (head l2)) =  ((l1++(head (my_merge (l2:l)))):(delete_firtst (my_merge (l2:l))))
+my_merge (l1:(l2:l)) | ((last l1)+1 == (head l2) || (last l1) == (head l2)+1) =  ((l1++(head (my_merge (l2:l)))):(delete_firtst (my_merge (l2:l))))
                      | otherwise = (l1:my_merge (l2:l))
 
 my_merge [x] = [x]
 
 delete_firtst (l:ls) = ls
+
+inside_flip ls 0 = []
+inside_flip (l:ls) n = ((reverse l):(inside_flip ls (n-1)))
+
+my_flip ls n = reverse (inside_flip ls n) ++ (drop n ls)
+
+
+which_flip [] _ _ = 0
+which_flip (x:xs) el n | (elem (el+1) x) || (elem (el-1) x) = n
+                       | otherwise = which_flip xs el (n+1)
+
+
+which_exac [] _ _ = 0
+which_exac (x:xs) el n | (elem el x)  = n
+                       | otherwise = which_exac xs el (n+1)
